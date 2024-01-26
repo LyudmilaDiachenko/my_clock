6120caf2f90685294c6c9b6a15e88d3ce9c52e8d
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2024-01-26 17:51:51 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Clock(props) {
+  return (
+    <div className='container'>
+      <h1>Годинник</h1>
+      <h2>{props.date.toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+function tick() {
+  root.render(<Clock date={new Date()} />);
+}
+
+setInterval(tick, 1000);
